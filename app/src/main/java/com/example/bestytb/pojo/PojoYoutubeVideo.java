a8c22c1560163eb8d4d0775daf9c605aa11414e0
mistheo <th.mistral@gmail.com>
2024-07-18 12:37:10 +0200

    oubli de l'implementation serializable

--- app/src/main/java/com/example/bestytb/pojo/PojoYoutubeVideo.java
+++ app/src/main/java/com/example/bestytb/pojo/PojoYoutubeVideo.java
@@ -4,8 +4,10 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.io.Serializable;
+
 @Entity
-public class PojoYoutubeVideo {
+public class PojoYoutubeVideo implements Serializable {
 
     @PrimaryKey(autoGenerate = true)
     private long id;
