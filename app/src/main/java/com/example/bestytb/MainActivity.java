19e70d4b216fccfd02c581913cab99ef828c1433
mistheo <th.mistral@gmail.com>
2024-07-18 13:01:42 +0200

    Ajout d'un menu via la toolbar, d'une activté pour l'ajout d'une video ytb

--- app/src/main/java/com/example/bestytb/MainActivity.java
+++ app/src/main/java/com/example/bestytb/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.bestytb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //Show Toolbar
+        Toolbar toolbarMainMenu = findViewById(R.id.toolbarMainMenu);
+        setSupportActionBar(toolbarMainMenu);
+    }
+
+
+
+    //Manage Toolbar Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.ItemMenuAddVideo) {
+            startActivity(new Intent(getApplicationContext(),));
+        }
     }
 }
